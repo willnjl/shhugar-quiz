fe8bc558718fdaa5377eca56616221976300943a
Fifteen Ten <fifteenten@Fifteens-MacBook-Pro.local>
2021-03-17 21:27:30 +0000

    items

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,20 +2,34 @@ import React, { useEffect } from "react";
 import { gsap } from "gsap";
 
 export default function Item({ q }) {
-  useEffect(() => {
-    let rotation = Math.random() + 1 * 4;
-    let tween = gsap.from("#item", {
-      rotate: rotation,
-      scale: 1.5,
-      ease: "bounce",
-      duration: 1.2,
-      yoyo: true,
-    });
-  }, [q]);
+  let rotation = Math.random() + 1 * 4;
+  // useEffect(() => {
+  //   // let tween = gsap.from("#item", {
+  //   //   rotate: rotation,
+  //   //   scale: 1.5,
+  //   //   ease: "bounce",
+  //   //   duration: 1.2,
+  //   //   yoyo: true,
+  //   // });
+  // }, []);
   return (
     <div className="wrap-tight">
       <div className="block item">
-        <img src={q.imgUrl} alt="" id={"item"} />
+        <img
+          src={q.imgUrl}
+          alt=""
+          id={"item"}
+          onLoad={() => {
+            console.log("load");
+            gsap.from("#item", {
+              rotate: rotation,
+              scale: 1.5,
+              ease: "bounce",
+              duration: 1.2,
+              yoyo: true,
+            });
+          }}
+        />
       </div>
     </div>
   );
