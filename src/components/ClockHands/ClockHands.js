58a5b38f9d696d35dac9c6acd9d2d8adb44b7f2e
Fifteen Ten <fifteenten@Fifteens-MacBook-Pro.local>
2021-03-24 17:34:24 +0000

    fixed clock hands bug

--- src/components/ClockHands/ClockHands.js
+++ src/components/ClockHands/ClockHands.js
@@ -1,31 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { gsap } from "gsap";
 
 const ClockHands = ({ hasAnswered }) => {
+  let handleHover = (tween, id) => {
+    if (!hands[id]) {
+      tween.restart();
+      hands[id] = true;
+      tween.eventCallback("onComplete", () => {
+        hands[id] = false;
+      });
+    }
+  };
+  const [hands, setHands] = useState({
+    1: false,
+    2: false,
+    3: false,
+  });
+  let tween1;
+  let tween2;
+  let tween3;
   useEffect(() => {
-    let tween = gsap.to(".animated-mins", {
-      rotate: 360,
-      duration: 6,
-      repeat: -1,
-    });
-    let tween2 = gsap.to(".animated-secs", {
-      rotate: 360,
-      duration: 5,
-      repeat: -1,
-      ease: "none",
-    });
-    let tween3 = gsap.to(".animated-hours", {
-      rotate: 360,
-      duration: 14,
-      repeat: -1,
-    });
+    tween1 = gsap
+      .to(".animated-secs", {
+        rotate: 360,
+        duration: 7,
+      })
+      .pause();
+    tween2 = gsap
+      .to(".animated-mins", {
+        rotate: 360,
+        duration: 2,
+      })
+      .pause();
+
+    tween3 = gsap
+      .to(".animated-hours", {
+        rotate: 360,
+        duration: 4,
+      })
+      .pause();
   }, []);
   return (
     <>
-      <div className={"animated-mins"}></div>
-      <div className={"animated-secs"}></div>
-      <div className={"animated-hours"}></div>
+      <div
+        className={"animated-secs"}
+        onMouseEnter={() => handleHover(tween1, 1)}
+      ></div>
+      <div
+        className={"animated-mins"}
+        onMouseEnter={() => handleHover(tween2, 2)}
+      ></div>
+      <div
+        className={"animated-hours"}
+        onMouseEnter={() => handleHover(tween3, 3)}
+      ></div>
       <div className={"animated-center"}></div>
     </>
   );
